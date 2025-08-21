39384026d46e52103fdbee4f54a66bb0232a8e1d
hailong289 <longdh2.dev@gmail.com>
2025-08-21 17:22:03 +0700

    feat: Add keyGenerator and skip options to rate limit middleware for enhanced request handling

--- backend/src/middleware/ratelimit.middleware.ts
+++ backend/src/middleware/ratelimit.middleware.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from "express";
 
 
 export class RateLimitMiddleware {
@@ -9,6 +10,8 @@ export class RateLimitMiddleware {
       max: 100, // Limit each IP to 100 requests per windowMs
       standardHeaders: 'draft-7', // Return rate limit info in the `RateLimit-*` headers
       legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+      keyGenerator: (req: Request, res: Response) => req.ip + "-" + req.headers["user-agent"],
+      skip: (req: Request, res: Response) => req.ip === "127.0.0.1", // Bỏ qua localhost
       message: {
         status: 429,
         error: 'Too many requests, please try again later.'
