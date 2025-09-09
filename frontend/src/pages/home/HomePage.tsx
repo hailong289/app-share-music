76d559545b658a94424d597db63b6161a9151e1a
haizgia <haizgia2912@gmail.com>
2025-09-09 20:46:33 +0700

    FE: home page: update name section

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -21,9 +21,9 @@ const HomePage = () => {
 			<ScrollArea className='h-[calc(100vh-180px)] w-full'>
 				<div className='p-4 sm:p-6'>
 					<div className='space-y-8'>
-					  <FeaturedSession title='Trending' session={featuredSession} isLoading={isLoading}/>
-					  <FeaturedSession title='Popular radio' session={popularRadio} isLoading={isLoading}/>
-					  <FeaturedSession title='Popular artist' session={popularArtist} isLoading={isLoading}/>
+					  <FeaturedSession title={featuredSession.name} session={featuredSession} isLoading={isLoading}/>
+					  <FeaturedSession title={popularRadio.name} session={popularRadio} isLoading={isLoading}/>
+					  <FeaturedSession title={popularArtist.name} session={popularArtist} isLoading={isLoading}/>
 					</div>
 				</div>
 			</ScrollArea>
